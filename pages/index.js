0faa92f9b9ee4be661edd4f4ef540c3e2c4afb2c
Victor Quiroz <victorhqc@gmail.com>
2019-01-05 13:37:03 -0600

    next.js Only index

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Index = () => (
+  <div>
+    <h1>Salón bugambilias</h1>
+    <h3>Sitio en construcción</h3>
+    <address>
+      Senda de los recuerdos 119, Milenio III, Querétaro, Qro 776060
+    </address>
+    <p>Contáctanos en <a href="tel:+524423138637">442 313 8637</a></p>
+    <p>O en <a href="ventas@bugambilias.party">ventas@bugambilias.party</a>
+    </p>
+  </div>
+);
+
+export default Index;
