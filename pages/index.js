af908f944f5d48c1ed72acd03ac98515697b7a7b
Victor Quiroz <victorhqc@gmail.com>
2019-01-06 15:48:54 -0600

    Update construction placeholder

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,38 @@
 import React from 'react';
-import styled from 'styled-components';
+import { Box, Typography } from '@smooth-ui/core-sc';
+import logo from '../public/salon_bugambilias_logo.png';
 
-const Container = styled.div`
-
-`;
+// C7B334
 
 const Index = () => (
-  <div>
-    <h1>Salón bugambilias</h1>
-    <h3>Sitio en construcción</h3>
-    <address>
-      Senda de los recuerdos 119, Milenio III, Querétaro, Qro 776060
-    </address>
-    <p>Contáctanos en <a href="tel:+524423138637">442 313 8637</a></p>
-    <p>O en <a href="ventas@bugambilias.party">ventas@bugambilias.party</a>
-    </p>
-  </div>
+  <Box
+    width={{ sm: 1 }}
+    display="flex"
+    justifyContent="center"
+    alignItems="center"
+    style={{
+      height: '100vh',
+      resize: 'both',
+      textAlign: 'center',
+    }}
+  >
+    <Box
+      width={{ sm: 0.8, md: 0.6 }}
+    >
+      <img alt="Salón bugambilias" src={logo} />
+      {/* <Typography variant="display-1">Salón bugambilias</Typography> */}
+      <Typography variant="display-4" color="primary">Sitio en construcción</Typography>
+      <address>
+        Senda de los recuerdos 119, Milenio III, Querétaro, Qro 776060
+      </address>
+      <p>
+        Contáctanos en <a href="tel:+524423138637">442 313 8637</a>
+      </p>
+      <p>
+        O en <a href="ventas@bugambilias.party">ventas@bugambilias.party</a>
+      </p>
+    </Box>
+  </Box>
 );
 
 export default Index;
