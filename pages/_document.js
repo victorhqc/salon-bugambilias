bc87f46209e1846afb3cd52746f847da5354f565
Victor Quiroz <victorhqc@gmail.com>
2019-01-06 20:24:05 -0600

    Add viewport

--- pages/_document.js
+++ pages/_document.js
@@ -21,6 +21,7 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           {/* Step 5: Output the styles in the head  */}
           {this.props.styleTags}
         </Head>
